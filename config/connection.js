ba8df61bd3f7fbaf8992dedae99c7f8eeb345637
Tony Tran <codemonkey713@gmail.com>
2021-03-15 21:02:53 -0500

    added the schema of database; server is up and running on listen

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,26 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const connectionURI = process.env.JAWSDB_URL
-    || `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:3306/${process.env.DB_NAME}`;
+let sequelize;
 
-const sequelize = new Sequelize(connectionURI);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+// const connectionURI = process.env.JAWSDB_URL
+//     || `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:3306/${process.env.DB_NAME}`;
+
+// const sequelize = new Sequelize(connectionURI);
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,5 +13,5 @@ app.use(routes);
 
 // FIXME: Turn force: true to force: false, these are not droids you are looking for
 sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on PORT:${PORT}`));
 });
