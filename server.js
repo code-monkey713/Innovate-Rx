f1adbff3e8a2bee46da26cfb1ba8e9b1bb47c467
Tony Tran <codemonkey713@gmail.com>
2021-03-15 21:22:36 -0500

    setting up build engine for heroku to take for a deployed site

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const exphbs = require('express-handlebars');
 const express = require('express');
 const routes = require('./controllers');
 
@@ -6,12 +8,18 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 // FIXME: Turn force: true to force: false, these are not droids you are looking for
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on PORT:${PORT}`));
 });
